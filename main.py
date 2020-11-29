129895347fc992d636c6f7afb22dc25df1596eb9
Márton Lovas <skynet.worldtakeover@gmail.com>
2020-11-29 20:28:43 +0100

    added hashtable to wordlist for fast lookup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 paths = [[y.split(',') for y in x.split(';')[:-1]] for x in open('paths.txt', 'r').readlines()[:-1]]
 letters = [x.split(',') for x in open('letters.txt', 'r', encoding = 'utf-8').read().split(';')]
-words = [x.strip('\n') for x in open('magyar_szavak.txt', 'r').readlines()]
+words = set([x.strip('\n') for x in open('magyar_szavak.txt', 'r').readlines()])
 
 for path in paths:
     word = ''
