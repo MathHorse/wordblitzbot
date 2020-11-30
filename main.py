9281aa88d61a80dfb903fdcb07fd84eb03dd3ddb
Márton Lovas <skynet.worldtakeover@gmail.com>
2020-11-30 11:12:56 +0100

    eliminiated double words

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from pynput.mouse import Button, Controller
 import time
 from pynput import keyboard
+import os
 mouse = Controller()
 
 def on_press(key):
     if key == keyboard.Key.shift:
-        exit()
+        os._exit(1)
 
 listener = keyboard.Listener(
     on_press=on_press)
@@ -18,11 +19,13 @@ mouse_coords = [[int(y) for y in x.split(',')] for x in open('mousepos.txt', 'r'
 print(mouse_coords)
 print(letters)
 
+voltmar = []
 for path in paths:
     word = ''
     for tile in path:
         word += letters[int(tile[0])][int(tile[1])]
-    if word in words:
+    if word in words and not word in voltmar:
+        voltmar.append(word)
         time.sleep(0.05)
         mouse.position = mouse_coords[4 * int(path[0][0]) + int(path[0][1])]
         mouse.press(Button.left)
